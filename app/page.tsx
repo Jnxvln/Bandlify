67eeecaba4027a74614b1654435150aa223b4051
Justin Cox <Justinkcox@ymail.com>
2023-10-12 15:35:12 -0500

    Change home page hero text color

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <header className="bg-gray-800 p-20">
-        <h1 className="text-5xl font-bold text-orange-400 text-center">Bandlify</h1>
+        <h1 className="text-5xl font-bold text-amber-400 text-center">Bandlify</h1>
         <h2 className="text-center text-white my-3">All your band resources in one place</h2>
       </header>
 
